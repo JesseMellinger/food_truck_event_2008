82b6b31f8ebd519f7bcabf8b9462e2a5f0a73908
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-15 14:58:59 -0600

    Create method to get total inventory, sorted item list, and tests

--- lib/event.rb
+++ lib/event.rb
@@ -20,4 +20,34 @@ class Event
     end
   end
 
+  def total_inventory
+    inventory_hash = {}
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, num_of_items|
+        if inventory_hash[item]
+          inventory_hash[item][:quantity] += num_of_items if inventory_hash[item]
+        else
+          inventory_hash[item] = {quantity: num_of_items, food_trucks: food_trucks_that_sell(item)}
+        end
+      end
+    end
+    inventory_hash
+  end
+
+  def sorted_item_list
+    item_names = []
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, num_of_items|
+        item_names << item.name if num_of_items > 0
+      end
+    end
+    item_names.uniq.sort
+  end
+
+  def overstocked_items
+    total_inventory.select do |item, hash|
+      hash[:quantity] > 50 && hash[:food_trucks].count > 1
+    end.keys
+  end
+
 end
