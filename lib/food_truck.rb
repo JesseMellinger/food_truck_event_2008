72420225d9e2aa03a56cc67e24518cf790a0ee62
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-15 13:21:29 -0600

    Create method to stock items and test

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -3,7 +3,15 @@ class FoodTruck
 
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, num_of_items)
+    @inventory[item] += num_of_items
   end
 
 end
